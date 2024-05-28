5bdb6efa9fd583f427fd710c7c541785b7b8bcd0
sJagstang <132593571+sJagstang@users.noreply.github.com>
2024-05-28 15:36:12 +0100

    Implemented Account Creation UI

--- app/src/main/java/com/example/newsapp/AccCreationActivity.kt
+++ app/src/main/java/com/example/newsapp/AccCreationActivity.kt
@@ -1,16 +1,24 @@
 package com.example.newsapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class AccCreationActivity : ComponentActivity() {
-    private lateinit var placeholderText: TextView
+    private lateinit var headerText: TextView
+    private lateinit var loginRedirect: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
-        placeholderText = findViewById(R.id.placeholderText)
+        headerText = findViewById(R.id.headerText)
+        loginRedirect = findViewById(R.id.loginRedirect)
+
+        loginRedirect.setOnClickListener {
+            val redirectIntent = Intent(this, LoginActivity::class.java)
+            startActivity(redirectIntent)
+        }
     }
 }
