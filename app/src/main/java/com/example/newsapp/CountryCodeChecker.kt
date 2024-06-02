b490f58c5519a425f067c843da907445a12fb7fd
sJagstang <132593571+sJagstang@users.noreply.github.com>
2024-06-02 12:52:04 +0100

    Location Setup for Region specific news

---
+++ app/src/main/java/com/example/newsapp/CountryCodeChecker.kt
@@ -0,0 +1,69 @@
+package com.example.newsapp
+
+// Matches the given country string from geocoder to its corresponding country code (ISO-3166 alpha2)
+class CountryCodeChecker {
+    fun checkCountryCode(country: String): String {
+        val countryCode = when (country) {
+            "United Arab Emirates" -> "ae"
+            "Argentina" -> "ar"
+            "Austria" -> "at"
+            "Australia" -> "au"
+            "Belgium" -> "be"
+            "Bulgaria" -> "bg"
+            "Brazil" -> "br"
+            "Canada" -> "ca"
+            "Switzerland" -> "ch"
+            "China" -> "cn"
+            "Columbia" -> "co"
+            "Cuba" -> "cu"
+            "Czech Republic" -> "cz"
+            "Germany" -> "de"
+            "Egypt" -> "eg"
+            "France" -> "fr"
+            "United Kingdom" -> "gb"
+            "Greece" -> "gr"
+            "Hong Kong" -> "hk"
+            "Hungary" -> "hu"
+            "Indonesia" -> "id"
+            "Ireland" -> "ie"
+            "Israel" -> "il"
+            "India" -> "in"
+            "Italy" -> "it"
+            "Japan" -> "jp"
+            "Republic of Korea" -> "kr"
+            "South Korea" -> "kr"
+            "Lithuania" -> "lt"
+            "Latvia" -> "lv"
+            "Morocco" -> "ma"
+            "Mexico" -> "mx"
+            "Malaysia" -> "my"
+            "Nigeria" -> "ng"
+            "Netherlands" -> "nl"
+            "Norway" -> "no"
+            "New Zealand" -> "nz"
+            "Philippines" -> "ph"
+            "Poland" -> "pl"
+            "Portugal" -> "pt"
+            "Romania" -> "ro"
+            "Serbia" -> "rs"
+            "Russia" -> "ru"
+            "Saudi Arabia" -> "sa"
+            "Sweden" -> "se"
+            "Singapore" -> "sg"
+            "Slovenia" -> "si"
+            "Slovakia" -> "sk"
+            "Thailand" -> "th"
+            "Türkiye" -> "tr"
+            "Turkey" -> "tr"
+            "Taiwan" -> "tw"
+            "Ukraine" -> "ua"
+            "United States" -> "us"
+            "Venezuela" -> "ve"
+            "South Africa" -> "za"
+
+            else -> "us" // Default case
+        }
+
+        return countryCode
+    }
+}
--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -47,26 +47,28 @@ class NewsActivity : ComponentActivity(), NewsTypeAdapter.OnItemClickListener {
     )
 
     private val apiRequests = APIRequests()
+    private val countryCodeChecker = CountryCodeChecker()
     private val db = Firebase.firestore
 
     private val apiKey = BuildConfig.NEWS_API_KEY
+    private var countryCode = "us" // Default case
 
+    // Requests permission from user to use their location for regional news
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
         if (isGranted) {
-            // Permission granted, proceed with your logic
-            getLastLocation()
+            getLastLocation() // If location is allowed, parse the location and get news based on that
         } else {
-            // Permission denied, handle accordingly
             Log.d(TAG, "Location permission denied")
+
+            loadTopStories("us", apiKey) // If region is not allowed, use default (US) location as a basis
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_newsfeed)
-
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
 
@@ -92,9 +94,6 @@ class NewsActivity : ComponentActivity(), NewsTypeAdapter.OnItemClickListener {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
 
-        // Loads the initial top stories on Activity start
-        loadTopStories(apiKey)
-
         // Gets user data from Firestore
         val docRef = db.collection("users").document(FirebaseAuth.getInstance().currentUser!!.uid)
 
@@ -119,8 +118,9 @@ class NewsActivity : ComponentActivity(), NewsTypeAdapter.OnItemClickListener {
         }
     }
 
-    private fun loadTopStories(apiKey: String) {
-        apiRequests.topStoriesRequest("us", apiKey) { response ->
+    // Loads the top stories for a given country based on the location retrieved
+    private fun loadTopStories(countryCode: String, apiKey: String) {
+        apiRequests.topStoriesRequest(countryCode, apiKey) { response ->
             runOnUiThread {
                 val articlesAdapter = ArticlesAdapter(response)
                 val newsRecyclerView: RecyclerView = findViewById(R.id.newsArticlesView)
@@ -131,11 +131,12 @@ class NewsActivity : ComponentActivity(), NewsTypeAdapter.OnItemClickListener {
         }
     }
 
+    // When one of the horizontal bar options for news type are selected
     override fun onItemClicked(position: Int, data: String) {
-        if (data == "Top Stories") {
-            loadTopStories(apiKey)
-        } else {
-            apiRequests.categoryRequests("us", data.lowercase(), apiKey) { response ->
+        if (data == "Top Stories") { // if top stories button is pressed, load top stories again
+            loadTopStories(countryCode, apiKey)
+        } else { // If any other button is pressed, category is selected
+            apiRequests.categoryRequests(countryCode, data.lowercase(), apiKey) { response -> // data is set to lowercase to match api fields
                 runOnUiThread {
                     val articlesAdapter = ArticlesAdapter(response)
                     val newsRecyclerView: RecyclerView = findViewById(R.id.newsArticlesView)
@@ -148,7 +149,7 @@ class NewsActivity : ComponentActivity(), NewsTypeAdapter.OnItemClickListener {
     }
 
     private fun getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(
+        if (ActivityCompat.checkSelfPermission( // Re-check location permission
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED &&
@@ -162,24 +163,37 @@ class NewsActivity : ComponentActivity(), NewsTypeAdapter.OnItemClickListener {
                     if (location != null) {
                         val geocoder = Geocoder(this, Locale.getDefault())
                         val addresses =
-                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1) // Get address based on lat/lon of location
 
                         if (!addresses.isNullOrEmpty()) {
-                            val country = addresses[0].countryName
-                            Log.d(TAG, "Country: $country")
+                            val country = addresses[0].countryName // Get the country name string from what geocoder provided
+
+                            // Converts from country name to country code for supported regions, if region is unsupported then defaults to US
+                            // For example, "United Kingdom" would return "gb" or China would return "cn"
+                            countryCode = countryCodeChecker.checkCountryCode(country)
+
+                            loadTopStories(countryCode, apiKey) // Load top stories based on the region retrieved
+
                         } else {
                             Log.d(TAG, "No address found for the provided coordinates.")
+
+                            loadTopStories(countryCode, apiKey) // Load with default (US) data if no region found
                         }
                     } else {
                         Log.d(TAG, "Location is null")
+
+                        loadTopStories(countryCode, apiKey) // Load with default (US) data if no region found
                     }
                 }
                 .addOnFailureListener { e ->
                     Log.e(TAG, "Error getting location: ${e.message}", e)
+
+                    loadTopStories(countryCode, apiKey) // Load with default (US) data if no region found
                 }
         }
     }
 
+    // Used to check if fine or coarse location permission has been given
     private fun checkPermissions(): Boolean {
         return (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
