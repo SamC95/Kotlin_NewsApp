63949c1976fcabd73688524d1b3d6d6f58242c4c
sJagstang <132593571+sJagstang@users.noreply.github.com>
2024-06-11 12:39:33 +0100

    Implemented Profile UI & Data Retrieval

--- app/src/main/java/com/example/newsapp/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/newsapp/activities/ProfileActivity.kt
@@ -3,21 +3,58 @@ package com.example.newsapp.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.newsapp.R
+import com.example.newsapp.adapters.ListDetailsAdapter
+import com.example.newsapp.adapters.SectionDetails
+import com.example.newsapp.data.UserDataManager
+import com.google.android.material.navigation.NavigationView
 
 class ProfileActivity : ComponentActivity() {
-    private lateinit var backButton: Button
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navHeaderText: TextView
+    private lateinit var navView: NavigationView
+    private lateinit var navButton: ImageButton
+
+    private val userDataManager = UserDataManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        backButton = findViewById(R.id.backButton)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        NavigationMenuListener.setListener(navView, this) // Sets up navigation menu buttons functionality
 
-        backButton.setOnClickListener {
-            val homeIntent = Intent(this, NewsActivity::class.java)
-            startActivity(homeIntent)
+        val headerView = navView.getHeaderView(0)
+        navHeaderText = headerView.findViewById(R.id.navHeaderText)
+        navButton = findViewById(R.id.nav_button)
+
+        // Sets up the user greeting on the side bar for this activity
+        userDataManager.setupGreeting(this, navHeaderText)
+
+        navButton.setOnClickListener {
+            drawerLayout.openDrawer(navView)
         }
+
+        val userIcon = R.drawable.user_24
+        val emailIcon = R.drawable.icons8_email_24
+        val bookmarkIcon = R.drawable.icons8_bookmark_24
+
+        val details = listOf(
+            SectionDetails(userIcon, "First Name", ""),
+            SectionDetails(userIcon, "Surname", ""),
+            SectionDetails(emailIcon, "Email Address", ""),
+            SectionDetails(bookmarkIcon, "Saved Articles", "")
+        )
+
+        val adapter = ListDetailsAdapter(this, details)
+
+        val listView: ListView = findViewById(R.id.list_view)
+        listView.adapter = adapter
     }
 }
